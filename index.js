820d35b217e2dfea8992a66ad65dfb2304e167c2
Raphaela664 <mahoraphy02@gmail.com>
2023-03-10 08:26:23 +0200

    chore: API Documentaion With Swagger

--- index.js
+++ index.js
@@ -5,9 +5,48 @@ const express = require('express')
 
 const app = express()
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express')
+
+
+const swaggerOptions={
+    swaggerDefinition :{
+        info:{
+            title: 'E-commerce API',
+            description: 'E-commerce API Information',
+            servers: [
+                {
+                  url: 'http://localhost:3000',
+                },
+              ]
+        }
+
+        
+    },
+
+    apis:['index.js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /home:
+ *  get:
+ *      description: Display homepage
+ *      responses:
+ *          '200':
+ *              description: successful request
+ */
+
 require('dotenv').config();
 
 const port = process.env.PORT;
+
+app.get('/home',(req,res)=>{
+    res.status(200).send("WELCOME!");
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
