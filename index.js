a399590cb95db50f4cd326171a2c7c605ae81c7d
ruberwa <ruberwa3@gmail.com>
2023-03-11 16:25:51 +0200

    running sample test

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
+// This code will test that the express server is running successfully and can 
+// fetch data about the current environment correctly via dotenv.
 
+const express = require('express')
 
-const port = process.env.PORT || 3000;
+const app = express()
+
+require('dotenv').config();
+
+const port = process.env.PORT;
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
+    console.log(`Server is running on port ${port}.`);
 });
-module.exports = app;
+
