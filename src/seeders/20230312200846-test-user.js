aafc15f89415ba73b1133bcfc5df18061a5af9e4
Bahati <bahatiyves10@gmail.com>
2023-03-14 09:24:00 +0200

    Updating changes from hound feedback

--- src/seeders/20230312200846-test-user.js
+++ src/seeders/20230312200846-test-user.js
@@ -1,53 +1,50 @@
 "use strict";
 
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Users', [
-      {
-        names: 'User Four',
-        email: 'userfour@gmail.com',
-        password: 'userfour',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        names: 'User One',
-        email: 'userone@gmail.com',
-        password: 'userone',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        names: 'User Two',
-        email: 'usertwo@gmail.com',
-        password: 'usertwo',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        names: 'User Three',
-        email: 'userthree@gmail.com',
-        password: 'userthree',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        names: 'Random user',
-        email: 'randomuser@gmail.com',
-        password: 'randomuser',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ]);
-  },
-
-  async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
-};
+export async function up(queryInterface, Sequelize) {
+  return queryInterface.bulkInsert('Users', [
+    {
+      names: 'User Four',
+      email: 'userfour@gmail.com',
+      password: 'userfour',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      names: 'User One',
+      email: 'userone@gmail.com',
+      password: 'userone',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      names: 'User Two',
+      email: 'usertwo@gmail.com',
+      password: 'usertwo',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      names: 'User Three',
+      email: 'userthree@gmail.com',
+      password: 'userthree',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      names: 'Random user',
+      email: 'randomuser@gmail.com',
+      password: 'randomuser',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ]);
+}
+export async function down(queryInterface, Sequelize) {
+  /**
+   * Add commands to revert seed here.
+   *
+   * Example:
+   * await queryInterface.bulkDelete('People', null, {});
+   */
+}
