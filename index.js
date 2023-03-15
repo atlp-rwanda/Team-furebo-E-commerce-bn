c6d6116e202da96522c16e9479f34a1c165a271c
Kalex <andelarwanda@Andelas-MacBook-Pro.local>
2023-03-15 22:20:18 +0200

    change

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 // This code will test that the express server is running successfully and can 
 // fetch data about the current environment correctly via dotenv.
 
-require('dotenv').config();
 const express = require('express')
+
 const app = express()
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express')
+
+
+const swaggerOptions={
+    swaggerDefinition :{
+        info:{
+            title: 'E-commerce API',
+            description: 'E-commerce API Information',
+            servers: [
+                {
+                  url: 'http://localhost:3000',
+                },
+              ]
+        }
+
+        
+    },
+
+    apis:['index.js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /home:
+ *  get:
+ *      description: Display homepage
+ *      responses:
+ *          '200':
+ *              description: successful request
+ */
+
+require('dotenv').config();
 
 const port = process.env.PORT;
 
