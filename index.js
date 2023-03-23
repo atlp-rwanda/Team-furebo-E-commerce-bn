d794ed0ba71e703c7aef42c72e764ec8445029b6
Kalex <andelarwanda@Andelas-MacBook-Pro.local>
2023-03-23 10:56:23 +0200

    changes

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-/* jshint esversion: 6 */
-
-// This code will test that the express server is running successfully and can 
+// This code will test that the express server is running successfully and can
 // fetch data about the current environment correctly via dotenv.
 
-const express = require('express');
-const app = express()
-require('dotenv').config()
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const passport = require("passport")
-require("./src/middleware/auth")
-const isLoggedIn = require("./src/middleware/isLoggedIn")
-const session = require('express-session');
-app.use(session({ secret: "cats" }));
-app.use(passport.initialize());
-app.use(passport.session());
+import express from 'express';
+import dotenv from 'dotenv';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import passport from 'passport';
+import './src/middleware/auth';
+import isLoggedIn from './src/middleware/isLoggedIn';
 
+const app = express();
+dotenv.config();
+const port = process.env.PORT;
 
+app.use(express.json());
 
 const swaggerOptions = {
   swaggerDefinition: {
@@ -46,59 +43,34 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
  *              description: successful request
  */
 
+app.get('/', (req, res) => {
+  res.status(200).send('<a href="/auth/google">Authenticate with Google</a>');
+});
 
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] })
+);
+
+app.get('/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/protected',
+    failureRedirect: '/auth/failure',
+  })
+);
+
+app.get('/auth/failure', (req, res) => {
+  res.send('Something went wrong');
+});
+
+app.get('/protected', isLoggedIn, (req, res) => {
+  res.status(200).send('Hello end user');
+});
 
 app.get('/home', (req, res) => {
   res.status(200).send('WELCOME!');
 });
 
-// const routes = require("./src/routes/app")
-
-
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-/**
- * @swagger
- * /home:
- *  get:
- *      description: Display homepage
- *      responses:
- *          '200':
- *              description: successful request
- */
-
-
-
-app.get("/", (req, res)=>{
-  res.status(200).send('<a href="/auth/google">Authenticate with Google</a>')
-})
-
-app.get("/auth/google",
-passport.authenticate("google", {scope: ['email', 'profile']})
-)
-
-app.get("/google/callback", 
-passport.authenticate('google', {
-  successRedirect:"/protected",
-  failureRedirect:"/auth/failure"
-})
-)
-app.get("/auth/failure",(req, res) =>{
-  res.send("Something went wrong")
-})
-
-app.get("/protected",isLoggedIn, (req,res)=>{
-  res.status(200).send("Hello end user")
-})
-
-app.get('/home', (req, res) => {
-  res.status(200).send('WELCOME!');
-});
-
-
-
-// set port for listening to the requests 
+// set port for listening to the requests
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}.`);
+  console.log(`Server is running on port ${port}.`);
 });
---
+++ src/routes/user.routes.js
@@ -0,0 +1,38 @@
+require("dotenv").config()
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const users = require("../controllers/user.controller.js");
+const router = require("express").Router();
+
+
+  // Create a new User
+  router.post("/create", users.create);
+  // // LOGIN
+  // router.post("/login", users.login);
+
+  // Retrieve all Users
+  router.get("/all", users.findAll);
+
+  // Retrieve a single User with id
+  router.get("/one/:id", users.findOne);
+
+  // Update a User with id
+  router.put("/update/:id", users.update);
+
+  // Delete a User with id
+  router.delete("/delete/:id", users.delete);
+
+  // Delete all Users
+  router.delete("/deleteall", users.deleteAll);
+
+
+
+router.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+
+router.get('/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
+  const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  res.json({ token });
+});
+
+module.exports=router;
