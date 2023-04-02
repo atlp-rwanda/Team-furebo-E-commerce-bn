5b7c9bc6fda43e1c0b3f08ee569732e27a1e9666
heshima18 <122611275+heshima18@users.noreply.github.com>
2023-04-02 16:33:23 +0200

    added coveralls

--- src/tests/addTowishList.test.js
+++ src/tests/addTowishList.test.js
@@ -2,10 +2,8 @@
 /* eslint-disable prefer-destructuring */
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import jwt from 'jsonwebtoken';
 import app from '../../index';
 import 'dotenv/config';
-import { User } from '../Database/models';
 
 chai.use(chaiHttp);
 
@@ -13,28 +11,25 @@ const { expect } = chai;
 
 describe('addToWishList', async () => {
   // Create a new user and get their authentication token
-  const d = await User.findOne({ where: { email: 'abc@gmail.com ' } });
-  const token = jwt.sign({ email: d.email, id: d.id }, process.env.USER_SECRET_KEY);
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFiY0BnbWFpbC5jb20iLCJpZCI6NCwiaWF0IjoxNjgwNDM3ODY1fQ.JaBy-rHIsEJsU71As12bFsoaWc7JkiioE689MvOL79w';
   const data = {
     token: token,
     productId: 1
   };
-
   context('check availablity of the user', () => {
     it('should return 404 if the user does not exist', async () => {
-      data.token = 'non-existent-id';
       const res = await chai.request(app)
         .post('/api/add-to-wishlist')
-        .send(data);
+        .send();
       expect(res).to.have.status(404);
     });
   });
   context('check availablity of the product', () => {
     it('should return 404 if the product does not exist', async () => {
-      data.productId = 'non-existent-id';
+      const p = data;
       const res = await chai.request(app)
         .post('/api/add-to-wishlist')
-        .send(data);
+        .send({ token: p.token, productId: null });
       const response = res.body;
       expect(res).to.have.status(404);
       expect(response.success).to.be.equal(false);
@@ -42,7 +37,7 @@ describe('addToWishList', async () => {
       console.log(data);
     });
   });
-  context('create a new wishlis', () => {
+  context('create a new wishlist', () => {
     it('should create a new wishlist if the user does not have one', async () => {
       const res = await chai.request(app)
         .post('/api/add-to-wishlist')
@@ -52,16 +47,16 @@ describe('addToWishList', async () => {
       expect(res.body.message).to.equal('Product added to wishlist');
     });
   });
-  context('remove the product from the wishlist', () => {
-    it('should remove the product from the wishlist if it is already in the wishlist', async () => {
+  context('add the product to the wishlist if it is not already available', () => {
+    it('should add the product to the wishlist if it is not already in the wishlist', async () => {
       const res = await chai.request(app)
         .post('/api/add-to-wishlist')
         .send(data);
       expect(res).to.have.status(200);
     });
   });
-  context('add the product to the wishlist if it is not already available', () => {
-    it('should add the product to the wishlist if it is not already in the wishlist', async () => {
+  context('remove the product from the wishlist', () => {
+    it('should remove the product from the wishlist if it is already in the wishlist', async () => {
       const res = await chai.request(app)
         .post('/api/add-to-wishlist')
         .send(data);
